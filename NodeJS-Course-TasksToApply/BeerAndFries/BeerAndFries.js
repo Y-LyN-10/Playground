08335fb31c9725dade1c98ab0e22db1bd0077970
Yulia Tenincheva <yulia.ten@dezigner.ru>
2014-09-26 01:20:46 +0300

    Modified to work the best in case amount of beers and fries is not equal

--- NodeJS-Course-TasksToApply/BeerAndFries/BeerAndFries.js
+++ NodeJS-Course-TasksToApply/BeerAndFries/BeerAndFries.js
@@ -6,6 +6,8 @@ exports.beerAndFries = function beerAndFries(items) {
         currentScore,
         maxSum = 0,
         pairs,
+        beers,
+        fries,
         i;
 
     items.sort(function (a, b) {
@@ -20,7 +22,13 @@ exports.beerAndFries = function beerAndFries(items) {
         }
     });
 
-    pairs = items.length / 2;
+    beers = beerScores.length;
+    fries = friesScores.length;
+
+    // If the amount of beers and fries is not equal, get only the max
+    // values of available pairs to score the result and ignore the rest
+    pairs = beers < fries ? beers : fries;
+
     for (i = 0; i < pairs; i += 1) {
         currentScore = beerScores[i] * friesScores[i];
         maxSum += currentScore;
