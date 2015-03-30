33653226f9bbbd7d0d7c1c8874e57bc80ff723a9
Yulia Tenincheva <yulia.ten@dezigner.ru>
2015-03-30 16:12:59 +0300

    TDD Today - Test all possible cases!

---
+++ week3/bank_account.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+
+class BankAccount:
+    def __init__(self, name, balance, currency):
+        
+        # Validations        
+        if BankAccountValidations.is_empty_string(name):
+            raise ValueError('Name should be at least 3 symbols')
+        
+        if not BankAccountValidations.is_valid_alpha_string(name):
+            raise ValueError('Name should contain only letters a-z and invervals')
+
+    self.__name = name
+    self.__balance = balance
+    self.__currency = currency
+
+    # TODO: That is not working
+    @property
+    def name(self):
+        return self.name;
+
+    @name.setter
+    def name(self, name):
+        name = re.sub(' +', ' ', name).strip()
+        self.__name = name;
+
+
+class BankAccountValidations:
+    def is_empty_string(string):
+        return len(string) < 3
+    
+    def is_valid_alpha_string(string):
+        for ch in string:
+            if not ch.isalpha():
+                if ch != ' ':
+                    return False
+                
+        return True
