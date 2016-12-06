2b349cd86d55014a66f37bb9666aa8bb3cd42688
Yulia Ten <Y-LyN-10@users.noreply.github.com>
2016-12-06 11:33:59 +0200

    Update 10-binary.cs

--- hackerrank-challanges/30in30/10-binary.cs
+++ hackerrank-challanges/30in30/10-binary.cs
@@ -4,35 +4,35 @@ public class Solution
 {
     private static int CountConsecutiveOnes(string binaryString)
     {	    
-		int counter = 0;
-		int maxCount = 0;
-		
-		for (int i = 0; i < binaryString.Length; i++)
-    	{	
-			if(binaryString[i] == '1'){
-				counter++;
-			} 
-		
-			if(counter > maxCount) {
-				maxCount = counter;
-			}
-		
-			if(binaryString[i] == '0') {
-				counter = 0;
-			}
+        int counter = 0;
+        int maxCount = 0;
+
+        for (int i = 0; i < binaryString.Length; i++)
+        {	
+            if(binaryString[i] == '1'){
+                counter++;
+            } 
+
+            if(counter > maxCount) {
+                maxCount = counter;
+            }
+
+            if(binaryString[i] == '0') {
+                counter = 0;
+            }
         }
 
-		return maxCount;	
+        return maxCount;	
     }
-    
+
     public static void Main(String[] args) 
     {
         int n = Convert.ToInt32(Console.ReadLine());
-        
-		string binaryN = Convert.ToString(n, 2);
-			
-		int result = CountConsecutiveOnes(binaryN);
 
-		Console.WriteLine(result);
+        string binaryN = Convert.ToString(n, 2);
+
+        int result = CountConsecutiveOnes(binaryN);
+
+        Console.WriteLine(result);
     }
 }
