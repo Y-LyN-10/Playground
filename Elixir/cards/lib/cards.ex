a11a58560f18c69d0694913e4f9567ce1a66fbc1
Yulia Tenincheva <tenincheva.yuli@ya.ru>
2019-04-30 17:44:58 +0300

    Completed the first two modules of elixir/phoenix course on udemy

---
+++ Elixir/cards/lib/cards.ex
@@ -0,0 +1,45 @@
+defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["spades", "Clubs", "Hearts", "Diamonds"]
+
+    for value <- values, suit <- suits do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    {hand, _rest_of_deck} = Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    # The "JavaScript" way, or how I would do it
+    # Cards.deal(Cards.shuffle(Cards.create_deck), hand_size)
+
+    # But in Elixir we have... pipes! So we can do that:
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+    
+  end
+end
