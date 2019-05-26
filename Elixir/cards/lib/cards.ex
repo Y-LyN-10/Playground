ed2bb0225fa1c27cf5d8f4dfb156a2cf711f3d64
Yulia Tenincheva <tenincheva.yuli@ya.ru>
2019-05-26 13:32:07 +0300

    Learned about doctest. I LOVE IT

--- Elixir/cards/lib/cards.ex
+++ Elixir/cards/lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """ 
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["spades", "Clubs", "Hearts", "Diamonds"]
@@ -12,10 +19,32 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of spades"]
+
+  """
   def deal(deck, hand_size) do
     {hand, _rest_of_deck} = Enum.split(deck, hand_size)
   end
