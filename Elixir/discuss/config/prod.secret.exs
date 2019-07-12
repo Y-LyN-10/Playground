65b866ccac128725e5075d177ee3f09ece047e31
Yulia Tenincheva <yulia.tenincheva@mentormate.com>
2019-07-12 13:54:42 +0300

    Added signin functionality

--- Elixir/discuss/config/prod.secret.exs
+++ Elixir/discuss/config/prod.secret.exs
@@ -17,12 +17,7 @@ config :discuss, Discuss.Repo,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+
 
 config :discuss, DiscussWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
--- Elixir/discuss/lib/discuss_web/controllers/auth_controller.ex
+++ Elixir/discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -1,9 +1,42 @@
 defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
+
+  alias Discuss.DiscussTopic.User
+  alias Discuss.Repo
+
   plug Ueberauth
 
-  def callback(conn, params) do
-    
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    user_attrs = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      provider: "Github"
+    }
+
+    changeset = User.changeset(%User{}, user_attrs)
+
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: Routes.topic_path(conn, :index))
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil -> Repo.insert(changeset)
+      user -> {:ok, user}
+    end
   end
 
 end
