70b93161790b3a6f1cb98768e9909f9cceed1f39
Yulia Tenincheva <yulia.ten@dezigner.ru>
2019-07-18 23:53:31 +0300

    Added functionality to add comments via websockets

--- Elixir/discuss/assets/js/socket.js
+++ Elixir/discuss/assets/js/socket.js
@@ -54,14 +54,23 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 // Finally, connect to the socket:
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("comments:1", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+const createSocket = (topicId) => {
+  // Now that you are connected, you can join channels with a topic:
+	let channel = socket.channel(`comments:${topicId}`, {})
+	channel.join()
+		.receive("ok", resp => {
+			console.log("Joined successfully", resp)
+		})
+		.receive("error", resp => {
+			console.log("Unable to join", resp)
+		})
 
-document.querySelector('button').addEventListener('click', function () {
-	channel.push('comment:hello', { hi: 'there' })
-})
+	document.querySelector('button').addEventListener('click', () => {
+		const content = document.querySelector('textarea').value;
+		channel.push('comment:add', { content: content });
+	});
+}
+
+window.createSocket = createSocket;
 
 export default socket
--- Elixir/discuss/lib/discuss_web/channels/comments_channel.ex
+++ Elixir/discuss/lib/discuss_web/channels/comments_channel.ex
@@ -1,14 +1,28 @@
 defmodule DiscussWeb.CommentsChannel do
   use DiscussWeb, :channel
 
-  def join(name, _params, socket) do
-    {:ok, %{hey: "there"}, socket}
+  alias Discuss.Repo
+  alias Discuss.DiscussTopic.{Topic, Comment}
+
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+
+    {:ok, %{}, assign(socket, :topic, topic)}
   end
 
-  def handle_in(name, message, socket) do
-    IO.puts('+++++++')
-    IO.puts(name)
-    IO.inspect(message)
-    {:reply, :ok, socket}
+  def handle_in(name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+
+    changeset = topic
+    |> Ecto.build_assoc(:comments)
+    |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        {:reply, :ok, socket}
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
