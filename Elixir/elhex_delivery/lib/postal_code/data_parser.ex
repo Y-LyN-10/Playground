429137958c22e4ca417f5e89806c7acdcf51876c
Yulia Tenincheva <yulia.tenincheva@mentormate.com>
2019-07-24 19:06:48 +0300

    Refactoring

--- Elixir/elhex_delivery/lib/postal_code/data_parser.ex
+++ Elixir/elhex_delivery/lib/postal_code/data_parser.ex
@@ -6,15 +6,21 @@ defmodule ElhexDelivery.PostalCode.DataParser do
     |> String.split("\n")
 
     data_rows
-    |> Enum.map(fn(row) -> String.split(row, "\s") end)
-    |> Enum.map(fn(row) ->
-      [_city, postal_code, latitude, longitude] = row
-
-      latitude = latitude |> String.replace(" ", "") |> String.to_float
-      longitude = longitude |> String.replace(" ", "") |> String.to_float
-
-      {postal_code, {latitude, longitude}}
-    end)
+    |> Enum.map(&(String.split(&1, "\s")))
+    |> Enum.map(&format_row(&1))
     |> Enum.into(%{})
+  end
+
+  defp parse_number(str) do
+    str |> String.replace(" ", "") |> String.to_float
+  end
+
+  # format multi-element list into a two element tuple
+  # [_city, postal_code, latitude, longitude] # => {postal_code, {latitude, longitude}}
+  defp format_row([_city, postal_code, latitude, longitude]) do
+    latitude = parse_number(latitude)
+    longitude = parse_number(longitude)
+
+    {postal_code, {latitude, longitude}}
   end
 end
